0b3074f50bbc12f00869792282943d1776904ea3
Mario Finelli <mario@finel.li>
2026-06-13 09:22:40 +0200

    Add initial landing page

--- vcheck.bash
+++ vcheck.bash
@@ -9,11 +9,22 @@ fi
 
 dver="$(grep LABEL Dockerfile | grep image.version | awk -F= '{print $2}')"
 cver="$(grep '^version' Cargo.toml | awk -F\" '{print $2}')"
+hver="$(grep '<span class="version">' index.html | awk -F\> '{print $2}' |
+  awk -F\< '{print $1}')"
 
 if [[ $dver != "v$cver" ]]; then
   echo >&2 "error: version mismatch"
 
-  echo "dockefile: $dver"
+  echo "dockerfile: $dver"
+  echo "cargo: $cver"
+
+  exit 1
+fi
+
+if [[ $hver != "v$cver" ]]; then
+  echo >&2 "error: version mismatch"
+
+  echo "index.html: $hver"
   echo "cargo: $cver"
 
   exit 1
